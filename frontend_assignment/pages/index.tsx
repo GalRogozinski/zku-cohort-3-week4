b86c312543f1e40172bbb63be72c4a911aad1c40
Gal Rogozinski <gal.rogozinski@iota.org>
2022-05-31 01:19:36 +0300

    week 4

--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -1,13 +1,42 @@
 import detectEthereumProvider from "@metamask/detect-provider"
+import { Box, Button, TextField } from "@mui/material"
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
 import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
-import { providers } from "ethers"
+import { Contract, providers, utils } from "ethers"
 import Head from "next/head"
-import React from "react"
+import React, { useState } from "react"
 import styles from "../styles/Home.module.css"
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import Greeter from "artifacts/contracts/Greeters.sol/Greeters.json"
+
+  const contract = new Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", Greeter.abi)
+  const provider = new providers.JsonRpcProvider("http://localhost:8545")
+  const contractOwner = contract.connect(provider.getSigner())
 
 export default function Home() {
     const [logs, setLogs] = React.useState("Connect your wallet and greet!")
+   
+    const schema = yup.object({
+        name: yup.string().required(),
+        age: yup.number().positive().integer().required(),
+        address: yup.string().required()
+      }).required();
+    const { control, handleSubmit, formState:{ errors } } = useForm({
+        resolver: yupResolver(schema)
+      });
+    const onSubmit = (data: any) => {
+        console.log(data);
+    }
+    const greeting = "Hello world"
+    const [greeti, setGreet] = useState("")
+
+    contractOwner.on("NewGreeting", (result) => {
+        let greetStr = utils.parseBytes32String(result)
+        setGreet(greetStr)
+        console.log(greet)
+    })
 
     async function greet() {
         setLogs("Creating your Semaphore identity...")
@@ -28,7 +57,6 @@ export default function Home() {
 
         setLogs("Creating your Semaphore proof...")
 
-        const greeting = "Hello world"
 
         const witness = Semaphore.genWitness(
             identity.getTrapdoor(),
@@ -57,7 +85,7 @@ export default function Home() {
         } else {
             setLogs("Your anonymous greeting is onchain :)")
         }
-    }
+    }   
 
     return (
         <div className={styles.container}>
@@ -74,8 +102,68 @@ export default function Home() {
 
                 <div className={styles.logs}>{logs}</div>
 
+                <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
+                    <Controller
+                        name="name"
+                        control={control}
+                        render={({ field: { onChange, value } }) => (
+                            <TextField 
+                                margin="normal"
+                                fullWidth
+                                required 
+                                onChange={onChange} 
+                                value={value || ''}
+                                label="Name" />
+                          )}
+                    />
+                    <p>{errors.name?.message}</p>
+                    <Controller
+                        name="age"
+                        control={control}
+                        render={({ field: { onChange, value } }) => (
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                label="Age"
+                                value={value || ''}
+                                onChange={onChange} 
+                                type="number"
+                            />
+                        )}
+                    />
+                    <p>{errors.age?.message}</p>
+                    <Controller
+                        name="address"
+                        control={control}
+                        render={({ field: { onChange, value } }) => (
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                label="Address"
+                                value={value || ''}
+                                onChange={onChange} 
+                                placeholder="100 Smallville, Kansas"
+                            />
+                        )}
+                    />
+                    <p>{errors.address?.message}</p>
+                    <Button
+                        onClick={handleSubmit(onSubmit)}
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >       
+                        Submit
+                    </Button>
+                </Box>
+
                 <div onClick={() => greet()} className={styles.button}>
                     Greet
+                </div>
+                <div>
+                    <p>{greeti}</p>
                 </div>
             </main>
         </div>
--- rust_assignment/freivald/src/main.rs
+++ rust_assignment/freivald/src/main.rs
@@ -1,37 +1,63 @@
+use std::ops::Mul;
 // TODO: Import necessary libraries. Check cargo.toml and the documentation of the libraries.
+use ark_bls12_381::{Fq, FQ_ONE};
+use ndarray::{Array2, Array1};
+use rand::random;
+
 struct Freivald {
-    x: // Array/Vec of Fq,
+    x: Array1<Fq>, // Array/Vec of Fq,
 }
 
 impl Freivald {
-    // TODO: Create constructor for object
     fn new(array_size: usize) -> Self {
-        todo!()
         // Generate random number
+        let r = random::<Fq>();
         // Populate vector with values r^i for i=0..matrix_size
+        let mut x: Vec<Fq> = Vec::with_capacity(array_size);
+        let mut vec_val: Fq = FQ_ONE;
+        for _i in 0..array_size {
+            x.push(vec_val);
+            vec_val = vec_val.mul(&r);
+        }
+        let xarr = Array1::from(x);
         // Return freivald value with this vector as its x value
+        Freivald { x: xarr }
     }
 
-    // TODO: Add proper types to input matrices. Remember matrices should hold Fq values
-    fn verify(&self, matrix_a, matrix_b, supposed_ab) -> bool {
-        assert!(check_matrix_dimensions(matrix_a, matrix_b, supposed_ab));
-        todo!()
-        // TODO: check if a * b * x == c * x. Check algorithm to make sure order of operations are
+    fn verify(&self, matrix_a: &Array2<Fq>, matrix_b: &Array2<Fq>, supposed_ab: &Array2<Fq>) -> bool {
+        assert!(check_matrix_dimensions(&matrix_a, &matrix_b, &supposed_ab));
+        // check if a * b * x == c * x. Check algorithm to make sure order of operations are
         // correct
+        let bx = matrix_b.dot(&self.x);
+        let abx = matrix_a.dot(&bx);
+        let cx = supposed_ab.dot(&self.x);
+        let abx_vec = abx as Array1<Fq>;
+        let cx_vec  = cx as Array1<Fq>;
+        return abx_vec.eq(&cx_vec);
     }
 
     // utility function to not have to instantiate Freivalds if you just want to make one
     // verification.
-    // TODO: Add types for arguments
-    fn verify_once(matrix_a, matrix_b, supposed_ab) -> bool {
+    fn verify_once(matrix_a: &Array2<Fq>, matrix_b: &Array2<Fq>, supposed_ab: &Array2<Fq>) -> bool {
         let freivald = Freivald::new(supposed_ab.nrows());
-        freivald.verify(matrix_a, matrix_b, supposed_ab)
+        return freivald.verify(matrix_a, matrix_b, supposed_ab);
     }
 }
+
 // TODO: [Bonus] Modify code to increase your certainty that A * B == C by iterating over the protocol.
 // Note that you need to generate new vectors for new iterations or you'll be recomputing same
 // value over and over. No problem in changing data structures used by the algorithm (currently its a struct
 // but that can change if you want to)
+pub fn verify_n(matrix_a:&Array2<Fq>, matrix_b:&Array2<Fq>, supposed_ab:&Array2<Fq>, n:u32) -> bool {
+    let mut success= true;
+    for _i in 0..n {
+        success = Freivald::verify_once(&matrix_a, &matrix_b, &supposed_ab);
+        if !success {
+            break;
+        }
+    }
+    success
+}
 
 
 // You can either do a test on main or just remove main function and rename this file to lib.rs to remove the
@@ -41,10 +67,18 @@ fn main() {
 }
 
 // TODO: Add proper types to input matrices. Remember matrices should hold Fq values
-pub fn check_matrix_dimensions(matrix_a, matrix_b, supposed_ab) -> bool {
-    // TODO: Check if dimensions of making matrix_a * matrix_b matches values in supposed_ab.
+pub fn check_matrix_dimensions(matrix_a: &Array2<Fq>, matrix_b: &Array2<Fq>, supposed_ab: &Array2<Fq>) -> bool {
     // If it doesn't you know its not the correct result independently of matrix contents
-    todo!()
+    let arows = matrix_a.nrows();
+    let acols = matrix_a.ncols();
+
+    let brows = matrix_b.nrows();
+    let bcols = matrix_b.ncols();
+
+    let abrows = supposed_ab.nrows();
+    let abcols = supposed_ab.ncols();
+
+    return acols == brows && bcols == abcols && arows == abrows;
 }
 
 #[cfg(test)]
@@ -52,40 +86,53 @@ mod tests {
     // #[macro_use]
     use lazy_static::lazy_static;
     use rstest::rstest;
+    use ndarray::arr2;
 
     use super::*;
 
     lazy_static! {
-        todo!("add matrices types and values")
-        static ref MATRIX_A: /* Type of matrix. Values should be fq */ = /* arbitrary matrix */;
-        static ref MATRIX_A_DOT_A: /* Type of matrix. Values should be fq */ = /* Correct result of A * A */;
-        static ref MATRIX_B: /* Type of matrix. Values should be fq */ = /* arbitrary matrix */;
-        static ref MATRIX_B_DOT_B: /* Type of matrix. Values should be fq */ = /* Correct result of B * B */;
-        static ref MATRIX_C: /* Type of matrix. Values should be fq */ = /* arbitrary LARGE matrix (at least 200, 200)*/;
-        static ref MATRIX_C_DOT_C: /* Type of matrix. Values should be fq */ = /* Correct result of C * C */;
+        static ref MATRIX_A:Array2<Fq> = arr2(&[[Fq::from(2), Fq::from(400), Fq::from(50)],
+                                                [Fq::from(123), Fq::from(6543), Fq::from(1)],
+                                                [Fq::from(2123), Fq::from(23), Fq::from(231)]]);
+        static ref MATRIX_A_DOT_A: Array2<Fq> = matrix_square(&MATRIX_A);
+        static ref MATRIX_B: Array2<Fq> = arr2(&[[Fq::from(4), Fq::from(1), Fq::from(1)],
+                                                [Fq::from(1), Fq::from(4), Fq::from(1)],
+                                                [Fq::from(1), Fq::from(45), Fq::from(3)]]);
+        static ref MATRIX_B_DOT_B: Array2<Fq> = matrix_square(&MATRIX_B);
+        static ref MATRIX_C: Array2<Fq> = create_matrix(200);
+        static ref MATRIX_C_DOT_C: Array2<Fq> = matrix_square(&MATRIX_C);
+    }
+
+    pub fn matrix_square(matrix: &Array2::<Fq>) -> Array2::<Fq> {
+        matrix.dot(matrix)
+    }
+
+    pub fn create_matrix(size: usize) -> Array2<Fq> {
+        Array2::ones((size,size))
     }
 
     #[rstest]
-    #[case(&MATRIX_A, &MATRIX_A, &MATRIX_A_DOT_A)]
-    #[case(&MATRIX_B, &MATRIX_B, &MATRIX_B_DOT_B)]
-    #[case(&MATRIX_C, &MATRIX_C, &MATRIX_C_DOT_C)]
+    #[case(& MATRIX_A, & MATRIX_A, & MATRIX_A_DOT_A)]
+    #[case(& MATRIX_B, & MATRIX_B, & MATRIX_B_DOT_B)]
+    #[case(& MATRIX_C, & MATRIX_C, & MATRIX_C_DOT_C)]
     fn freivald_verify_success_test(
-        #[case] matrix_a: /* Type of matrix. Values should be fq */,
-        #[case] matrix_b: /* Type of matrix. Values should be fq */,
-        #[case] supposed_ab: /* Type of matrix. Values should be fq */,
+        #[case] matrix_a: &Array2<Fq>,
+        #[case] matrix_b: &Array2<Fq>,
+        #[case] supposed_ab: &Array2<Fq>,
     ) {
         let freivald = Freivald::new(supposed_ab.nrows());
         assert!(freivald.verify(matrix_a, matrix_b, supposed_ab));
+        assert!(verify_n(matrix_a, matrix_b, supposed_ab, 5));
     }
 
     #[rstest]
-    #[case(&MATRIX_A, &MATRIX_B, &MATRIX_A_DOT_A)]
-    #[case(&MATRIX_B, &MATRIX_A, &MATRIX_B_DOT_B)]
-    #[case(&MATRIX_C, &MATRIX_B, &MATRIX_C_DOT_C)]
+    #[case(& MATRIX_A, & MATRIX_B, & MATRIX_A_DOT_A)]
+    #[case(& MATRIX_B, & MATRIX_A, & MATRIX_B_DOT_B)]
+    #[case(& MATRIX_C, & MATRIX_B, & MATRIX_C_DOT_C)]
     fn freivald_verify_fail_test(
-        #[case] a: /* Type of matrix. Values should be fq */,
-        #[case] b: /* Type of matrix. Values should be fq */,
-        #[case] c: /* Type of matrix. Values should be fq */,
+        #[case] a: &Array2<Fq>,
+        #[case] b: &Array2<Fq>,
+        #[case] c: &Array2<Fq>,
     ) {
         let freivald = Freivald::new(c.nrows());
         assert!(!freivald.verify(a, b, c));
